0625186741d58b2d51bdd5dacb75adf34e50ad00
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2013-05-26 16:50:01 -0300

    Some visual tweaks.

--- game/main.lua
+++ game/main.lua
@@ -17,12 +17,12 @@ function love.load ()
   end
   ui.board.defineZone(1,1,1,7, {50,50,110,255})
   ui.board.defineZone(6,2,6,8, {50,50,110,255})
-  ui.board.defineZone(2,2,3,7, {90,55,40,255})
-  ui.board.defineZone(4,2,5,7, {40,80,55,255})
+  ui.board.defineZone(2,1,3,7, {90,55,40,255})
+  ui.board.defineZone(4,2,5,8, {40,80,55,255})
+  ui.board.defineZone(4,1,5,1, {20,40,27,255})
+  ui.board.defineZone(2,8,3,8, {45,27,20,255})
   ui.board.defineZone(1,8,1,8, {40,40,55,255}, true)
   ui.board.defineZone(6,1,6,1, {40,40,55,255}, true)
-  ui.board.defineZone(2,1,5,1, {110,100,55,255})
-  ui.board.defineZone(2,8,5,8, {110,100,55,255})
 end
 
 function love.mousereleased (x, y, button)
--- game/ui/card.lua
+++ game/ui/card.lua
@@ -31,13 +31,13 @@ function Card:draw (graphics, hidden, selection)
   if self.tapped then
     graphics.rotate(math.pi/2)
   end
-  if selection[self] then
-    graphics.setColor(100,180,140,255)
-    graphics.rectangle('fill', -36, -52, 72, 104)
-  end
   graphics.setColor(90,70,50,255)
   graphics.rectangle('fill', -32, -48, 64, 96)
-  graphics.setColor(255,255,255,255)
+  if selection[self] then
+    graphics.setColor(100,180,140,255)
+  else
+    graphics.setColor(255,255,255,255)
+  end
   graphics.rectangle('line', -32, -48, 64, 96)
   graphics.setColor(100,140,180,255)
   if not hidden and self.info then
